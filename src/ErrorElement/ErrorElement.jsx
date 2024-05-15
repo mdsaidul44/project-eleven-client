f2916a3b11afc53f33811f2c5282b6524e6f3ff9
mdsaidul44 <islam88758@gmail.com>
2024-05-15 16:16:48 +0600

    error page design done

--- src/ErrorElement/ErrorElement.jsx
+++ src/ErrorElement/ErrorElement.jsx
@@ -1,14 +1,22 @@
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
- 
+
 const ErrorElement = () => {
-    useEffect(()=>{
-        document.title  = "Home | date Not Found"
-    },[])
+    useEffect(() => {
+        document.title = "Home | date Not Found"
+    }, [])
     return (
-        <div>
-            <Link to='/'><button className="btn">Back to home</button></Link>
+        <div> 
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content text-center">
+                    <div className="max-w-md">
+                        <h1 className="text-9xl font-bold">Oops!</h1>
+                        <p className="py-6 text-2xl"><span className="text-4xl font-bold">404</span>  Data Not Found</p>
+                        <Link to='/'><button className="btn bg-green-600">Go To Home</button></Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
